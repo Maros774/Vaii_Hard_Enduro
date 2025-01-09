ad9dbe08fd298eb8fddb1c6a3a8a342bedba1f56
Maroš Rončák <roncak8@stud.uniza.sk>
2025-01-09 10:49:29 +0100

    - Uprava vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: 'localhost',
-        port: 5173, // port na ktorom Vite beží
-    },
-});
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
 
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+        server: {
+            host: 'localhost',
+            port: 5173,
+        },
+        define: {
+            'process.env.APP_URL': env.APP_URL,
+        },
+    };
+});
